daf0ac004fceade053ae3c8ad12215f82f5b0793
Sid Pandey <sid2065@gmail.com>
2024-06-26 13:39:03 +0200

    Added input form for first fermentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBarHeader from "./components/navigation/NavBarHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Splash",
-  description: "Splash is your ultimate kombucha companion app, designed to help you track and manage your kombucha drinking habits. Stay refreshed and healthy with Splash!",
+  description:
+    "Splash is your ultimate kombucha companion app, designed to help you track and manage your kombucha drinking habits. Stay refreshed and healthy with Splash!",
 };
 
 export default function RootLayout({
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBarHeader fixed />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,28 @@
-import Image from "next/image";
 import Link from "next/link";
+
+import NavBarHeader from "./components/navigation/NavBarHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-faded-yellow">
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <Link
-          href="/dashboard"
-          className=" group rounded-lg border border-transparent px-5 py-4 transition-colors
+    <>
+      <NavBarHeader fixed />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-faded-yellow">
+        <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+          <Link
+            href="/dashboard"
+            className=" group rounded-lg border border-transparent px-5 py-4 transition-colors
 
-                      bg-faded-black text-white hover:shadow-xl
-                      "
-          //target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold ">
-            Make your first batch
-          </h2>
-        </Link>
-      </div>
-    </main>
+                        bg-faded-black text-white hover:shadow-xl
+                        "
+            //target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold ">
+              Make your first batch
+            </h2>
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
